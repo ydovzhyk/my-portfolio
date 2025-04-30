b5db849c4c1175755b41bdc88b6194e5ffd8e37b
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-04-30 22:07:27 +0300

    add corrections

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -1,17 +1,18 @@
 // @flow strict
 'use client';
 
-import { useIsClient } from '../../helper/useIsClient';
 import { educations } from '../../../../utils/data/educations';
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
-// import lottieFile from '../../../assets/lottie/study.json';
+import dynamic from 'next/dynamic'
+import lottieFile from '../../../assets/lottie/study.json';
 // import AnimationLottie from "../../helper/animation-lottie";
+const AnimationLottie = dynamic(() => import('../../helper/animation-lottie'), {
+  ssr: false,
+})
 import GlowCard from "../../helper/glow-card";
 
 function Education() {
-  const isClient = useIsClient();
-  console.log('isClient', isClient);
 
   return (
     <section
@@ -43,9 +44,9 @@ function Education() {
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="flex justify-center items-start">
-          {/* <div className="w-3/4 h-3/4">
+          <div className="w-3/4 h-3/4">
             <AnimationLottie animationPath={lottieFile} />
-          </div> */}
+          </div>
         </div>
 
         <div className="flex flex-row items-center justify-center">
