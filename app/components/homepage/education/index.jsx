b3073ca9b22149fa5630ab0b06606ea4d42605c0
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-04-30 22:16:32 +0300

    add corrections

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -2,17 +2,15 @@
 'use client';
 
 import { educations } from '../../../../utils/data/educations';
+import { useIsClient } from '../../helper/useIsClient';
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
-import dynamic from 'next/dynamic'
 import lottieFile from '../../../assets/lottie/study.json';
-// import AnimationLottie from "../../helper/animation-lottie";
-const AnimationLottie = dynamic(() => import('../../helper/animation-lottie'), {
-  ssr: false,
-})
+import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
 
 function Education() {
+  const isClient = useIsClient();
 
   return (
     <section
@@ -45,44 +43,42 @@ function Education() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="flex justify-center items-start">
           <div className="w-3/4 h-3/4">
-            <AnimationLottie animationPath={lottieFile} />
+            {isClient ? <AnimationLottie animationPath={lottieFile} /> : null}
           </div>
         </div>
 
         <div className="flex flex-row items-center justify-center">
           <div className="flex flex-col gap-5">
-            {educations.map(
-              (education) => (
-                <GlowCard
-                  key={education.id}
-                  identifier={`education-${education.id}`}
+            {educations.map((education) => (
+              <GlowCard
+                key={education.id}
+                identifier={`education-${education.id}`}
+              >
+                <div
+                  className="p-2 relative bg-no-repeat bg-[length:100%_100%] bg-bottom"
+                  style={{ backgroundImage: "url('/blur-23.svg')" }}
                 >
-                  <div
-                    className="p-2 relative bg-no-repeat bg-[length:100%_100%] bg-bottom"
-                    style={{ backgroundImage: "url('/blur-23.svg')" }}
-                  >
-                    <div className="flex justify-center">
-                      <p className="text-xs sm:text-sm text-[#16f2b3]">
-                        {education.duration}
+                  <div className="flex justify-center">
+                    <p className="text-xs sm:text-sm text-[#16f2b3]">
+                      {education.duration}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-x-8 px-3 py-4">
+                    <div className="text-violet-500  transition-all duration-300 hover:scale-125">
+                      <BsPersonWorkspace size={36} />
+                    </div>
+                    <div>
+                      <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                        {education.title}
+                      </p>
+                      <p className="text-sm sm:text-base">
+                        {education.institution}
                       </p>
                     </div>
-                    <div className="flex items-center gap-x-8 px-3 py-4">
-                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                        <BsPersonWorkspace size={36} />
-                      </div>
-                      <div>
-                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                          {education.title}
-                        </p>
-                        <p className="text-sm sm:text-base">
-                          {education.institution}
-                        </p>
-                      </div>
-                    </div>
                   </div>
-                </GlowCard>
-              )
-            )}
+                </div>
+              </GlowCard>
+            ))}
           </div>
         </div>
       </div>
