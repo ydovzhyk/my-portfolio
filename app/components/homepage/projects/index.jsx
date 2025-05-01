2e06d2cb7b5f79eb4ade75302a2c1006694401d0
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2025-05-01 18:56:39 +0300

    add card project corrections
    add card project corrections

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,17 +1,60 @@
-import { projectsData } from '../../../../utils/data/projects-data';
-import ProjectCard from './project-card';
+// import { projectsData } from '../../../../utils/data/projects-data';
+// import ProjectCard from './project-card';
+
+// const Projects = () => {
+
+//   return (
+//     <section
+//       id="projects"
+//       className="relative z-50 my-12 lg:my-24 min-h-[700px]"
+//     >
+//       <div className="sticky top-10">
+//         <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+//         <div className="flex items-center justify-start relative">
+//           <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+//             PROJECTS
+//           </span>
+//           <span className="w-full h-[2px] bg-[#1a1443]"></span>
+//         </div>
+//       </div>
+
+//       <div className="pt-24">
+//         <div className="flex flex-col gap-6">
+//           {projectsData.slice(0, 4).map((project, index) => (
+//             <div
+//               id={`sticky-card-${index + 1}`}
+//               key={index}
+//               className="sticky-card w-full mx-auto max-w-2xl sticky"
+//             >
+//               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
+//                 <ProjectCard project={project} />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   )
+// };
+
+// export default Projects;
+
+import { projectsData } from '../../../../utils/data/projects-data'
+import ProjectCard from './project-card'
 
 const Projects = () => {
+  const baseTop = 100
+  const headerHeight = 0
 
   return (
     <section
       id="projects"
       className="relative z-50 my-12 lg:my-24 min-h-[700px]"
     >
-      <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+      <div className="sticky top-10 z-20">
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
         <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+          <span className="bg-[#1a1443] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">
             PROJECTS
           </span>
           <span className="w-full h-[2px] bg-[#1a1443]"></span>
@@ -19,12 +62,13 @@ const Projects = () => {
       </div>
 
       <div className="pt-24">
-        <div className="flex flex-col gap-6">
-          {projectsData.slice(0, 4).map((project, index) => (
+        <div className="flex flex-col gap-40 relative">
+          {projectsData.map((project, index) => (
             <div
               id={`sticky-card-${index + 1}`}
               key={index}
-              className="sticky-card w-full mx-auto max-w-2xl sticky"
+              className="sticky z-10 w-full mx-auto max-w-2xl"
+              style={{ top: `${baseTop + index * headerHeight}px` }}
             >
               <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
                 <ProjectCard project={project} />
@@ -35,6 +79,7 @@ const Projects = () => {
       </div>
     </section>
   )
-};
+}
 
 export default Projects;
+
--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -1,9 +1,7 @@
 // @flow strict
-
-import * as React from 'react';
+import * as React from 'react'
 
 function ProjectCard({ project }) {
-
   return (
     <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full">
       <div className="flex flex-row">
@@ -20,7 +18,7 @@ function ProjectCard({ project }) {
           {project.name}
         </p>
       </div>
-      <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+      <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8 h-[440px] flex flex-col justify-start">
         <code className="font-mono text-xs md:text-sm lg:text-base">
           <div className="blink">
             <span className="mr-2 text-pink-500">const</span>
@@ -29,43 +27,86 @@ function ProjectCard({ project }) {
             <span className="text-gray-400">{'{'}</span>
           </div>
           <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-            <span className="text-gray-400">{`'`}</span>
-            <span className="text-amber-300">{project.name}</span>
-            <span className="text-gray-400">{`',`}</span>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">'name'</span>:
+            <span className="text-gray-400"> '</span>
+            <span className="text-orange-200">{project.name}</span>
+            <span className="text-gray-400">',</span>
           </div>
-
           <div className="ml-4 lg:ml-8 mr-2">
-            <span className=" text-white">tools:</span>
-            <span className="text-gray-400">{` ['`}</span>
-            {
-              project.tools.map((tag, i) => (
-                <React.Fragment key={i}>
-                  <span className="text-amber-300">{tag}</span>
-                  {
-                    project.tools?.length - 1 !== i &&
-                    <span className="text-gray-400">{`', '`}</span>
-                  }
-                </React.Fragment>
-              ))
-            }
-            <span className="text-gray-400">{"],"}</span>
+            <span className=" text-white">'tools'</span>:
+            <span className="text-gray-400"> [</span>
+            {project.tools.map((tag, i) => (
+              <React.Fragment key={i}>
+                <span className="text-orange-200">'{tag}'</span>
+                {project.tools.length - 1 !== i && (
+                  <span className="text-gray-400">, </span>
+                )}
+              </React.Fragment>
+            ))}
+            <span className="text-gray-400">],</span>
           </div>
           <div>
-            <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
-            <span className="text-orange-400">{project.role}</span>
+            <span className="ml-4 lg:ml-8 mr-2 text-white">'myRole'</span>:
+            <span className="text-orange-400"> '{project.role}'</span>
             <span className="text-gray-400">,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
-            <span className="text-white">Description:</span>
-            <span className="text-cyan-400">{' ' + project.description}</span>
+            <span className="text-white">'description'</span>:
+            <span className="text-cyan-400"> '{project.description}'</span>
             <span className="text-gray-400">,</span>
           </div>
-          <div><span className="text-gray-400">{`};`}</span></div>
+          {project.frontendGitLink && (
+            <div className="ml-4 lg:ml-8 mr-2">
+              <span className="text-white">'frontendCode'</span>:&nbsp;
+              <a
+                href={project.frontendGitLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 no-underline hover:underline"
+              >
+                link
+              </a>
+              <span className="text-gray-400">,</span>
+            </div>
+          )}
+          {project.backendGitLink && (
+            <div className="ml-4 lg:ml-8 mr-2">
+              <span className="text-white">'backend-code'</span>:&nbsp;
+              <a
+                href={project.backendGitLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 no-underline hover:underline"
+              >
+                link
+              </a>
+              <span className="text-gray-400">,</span>
+            </div>
+          )}
+          {project.demo && (
+            <div className="ml-4 lg:ml-8 mr-2">
+              <span className="text-white">'preview'</span>:&nbsp;
+              <a
+                href={project.demo}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 no-underline hover:underline"
+              >
+                link
+              </a>
+              <span className="text-gray-400">,</span>
+            </div>
+          )}
+          <div>
+            <span className="text-gray-400">{'};'}</span>
+          </div>
         </code>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default ProjectCard;
+
+
+
